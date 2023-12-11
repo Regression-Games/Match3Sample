da3a61f828b91d8babdeb7091caea65c6796bf9b
Ashley Stanton-Nurse <git@analogrelay.net>
2023-12-11 14:09:50 -0800

    [REG-1495] Update reference to 'gg.regression.unity.bots'

---
+++ Assets/RegressionGames/Runtime/GeneratedScripts/RGActionMap.cs
@@ -0,0 +1,17 @@
+/*
+* This file has been automatically generated. Do not modify.
+*/
+
+using System;
+
+namespace RGCutie_Tutti_Frutti___Match_3
+{
+    using UnityEngine;
+
+    public class RGActionMap : MonoBehaviour
+    {
+        private void Awake()
+        {
+        }
+    }
+}
---
+++ Assets/RegressionGames/Runtime/GeneratedScripts/RGSerialization.cs
@@ -0,0 +1,15 @@
+/*
+* This file has been automatically generated. Do not modify.
+*/
+
+using System;
+using Newtonsoft.Json;
+
+namespace RGCutie_Tutti_Frutti___Match_3
+{
+    using UnityEngine;
+
+    public static class RGSerialization
+    {
+    }
+}
